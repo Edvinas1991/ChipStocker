50136ff4217f39afda3137ee3018c7954691aaab
eadwine91 <30504915+Edvinas1991@users.noreply.github.com>
2021-12-23 20:25:11 +0200

    ADD User Dto

--- chipStocker-Api/src/main/java/lt/codeacademy/chipstockerapi/controller/LoginController.java
+++ chipStocker-Api/src/main/java/lt/codeacademy/chipstockerapi/controller/LoginController.java
@@ -1,8 +1,12 @@
 package lt.codeacademy.chipstockerapi.controller;
 
+import lt.codeacademy.chipstockerapi.dto.UserDto;
+import lt.codeacademy.chipstockerapi.entity.User;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 import static lt.codeacademy.chipstockerapi.ApiPath.LOGIN;
 
 @RestController
@@ -10,6 +14,7 @@ import static lt.codeacademy.chipstockerapi.ApiPath.LOGIN;
 public class LoginController {
 
     @PostMapping
-    public void login() {
+    public UserDto login(@AuthenticationPrincipal User user) {
+        return new UserDto(user);
     }
 }
